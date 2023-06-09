93d421dd7833cfad350c562be01b56a58b2e743b
Carlos Valdez <cvaldezh@uci.edu>
2023-06-09 00:02:36 -0700

    feat: untested api changes

--- src/cvaldez/access/api.py
+++ src/cvaldez/access/api.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, Response
-from .SiteTools.user import User, Token
-from .SiteTools.exceptions import UserNotFoundError, InvalidTokenError, DuplicateUsernameError, UsernameRuleError, PasswordRuleError
+from access import Identity, IdentityEditor, Token
+from access import UserNotFoundError, DuplicateUsernameError, UsernameRuleError, PasswordRuleError, InvalidTokenError
 import json
 
 bp = Blueprint('access api', __name__,
@@ -9,7 +9,16 @@ bp = Blueprint('access api', __name__,
                url_prefix='/api/access/')
 
 
-@bp.route('/info/')
+@bp.route('/account-exists/', methods=['POST'])
+def api_check_account():
+    data = json.loads(request.data)
+    if 'username' in data:
+        return Response(json.dumps({"exists": Identity.exists(data['username'])}), status=200)
+    else:
+        return Response(json.dumps({'exists': False}), status=401)
+
+
+@bp.route('/verify-login/')
 def api_account_info():
     """
     Gets information about the logged-in user.
@@ -36,7 +45,7 @@ def api_account_update():
     try:
         # Identifies the user through their token
         token = Token(request.headers['Bearer'])
-        user = token.owner()
+        user = token.owner(editor=True)
 
         # If the user has TOTP, make sure they logged in using 2FA
         if user.has_totp() and token.type() != 'T':
@@ -45,7 +54,7 @@ def api_account_update():
 
         if 'username' in data:
             try:
-                user.update(username=data['username'])
+                user.set_username(username=data['username'])
                 return json.dumps({"message": "Successfully updated username!"})
             except DuplicateUsernameError:
                 return Response(json.dumps(
@@ -57,13 +66,12 @@ def api_account_update():
                 }, status=401))
         elif 'password' in data:
             try:
-                user.update(password=data['password'])
+                user.set_password(password=data['password'])
                 return json.dumps({"message": "Successfully updated password!"})
             except PasswordRuleError:
                 return Response({"message": "ERROR: Password violated Password Rules."}, status=401)
         else:
             return json.dumps({"message": "ERROR: Only usernames and passwords can be updated."})
-
 
     except InvalidTokenError:
         return json.dumps({"message": "ERROR: Could not confirm login."})
@@ -77,7 +85,7 @@ def api_account_login():
         # This is where account creation happens
         # Ensures the Password and Username rules are followed
         try:
-            user = User.create(data['username'], data['password'])
+            user = IdentityEditor.create(data['username'], data['password'])
 
             return Response(json.dumps({
                 "token": str(user.generate_token('P')),
@@ -106,28 +114,21 @@ def api_account_login():
     else:
         # This is where logging in happens
         try:
-            user = User(data['username'])
-
             # Verifies password
-            if user.check_password(data['password']):
-                return json.dumps({
-                    "token": str(user.generate_token('P')),
-                    "totp_required": user.has_totp(),
-                    "message": None
-                }, indent=2)
-            else:
-                return Response(json.dumps({
-                    "token": None,
-                    "totp_required": False,
-                    "message": "ERROR: Password is incorrect. Please try again."
-                }), status=401)
+            user = Identity(username=data['username']).editor(data['password'])
+            return json.dumps({
+                "token": str(user.generate_token('P')),
+                "totp_required": user.has_totp(),
+                "message": None
+            }, indent=2)
+
         except UserNotFoundError:
             return Response(json.dumps({
-                "message": "ERROR: Username not found."
+                "message": "ERROR: Username or password is not correct.."
             }), status=401)
 
 
-@bp.route('/totp/', methods=['GET', 'POST'])
+@bp.route('/totp/', methods=['POST'])
 def api_account_totp():
     try:
         # Verifies the token exists
@@ -136,38 +137,12 @@ def api_account_totp():
         return Response(json.dumps(
             {"message": "ERROR: Could not verify login."}
         ), status=401)
-    # Identify user using token
-    user = token.owner()
 
-    if request.method == "GET":
-        """
-        This is for setting up TOTP.
+    user = token.owner(editor=True)
+    data = json.loads(request.data)
+    success = user.check_totp(data['code'])
 
-        :returns: A setup code
-        """
-
-        # If the user has TOTP, make sure they logged in using 2FA
-        if user.has_totp() and token.type() != 'T':
-            return Response({"message": 'ERROR: Token must be of type T if they have a totp.'},
-                            status=401)
-
-        return json.dumps({"setup_code": user.generate_2fa_base32()})
-    elif request.method == "POST":
-        """
-        This is where 2FA happens.
-
-        :returns: Whether or not it worked
-        """
-        data = json.loads(request.data)
-        if data['setup']:
-            success = user.save_2fa(data['code'])
-            return json.dumps(
-                {"success": success,
-                 "token": None if not success else str(user.generate_token('T'))}
-            )
-        else:
-            success = user.check_totp(data['code'])
-            return json.dumps(
-                {"success": success,
-                 "token": None if not success else str(user.generate_token('T'))}
-            )
+    return json.dumps(
+        {"success": success,
+         "token": None if not success else str(user.generate_token('T'))}
+    )
